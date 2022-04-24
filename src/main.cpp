dae574616efde4e7b4eb682604cbe0dce49e2ed0
kadeska <kadeska23@gmail.com>
2022-04-23 23:03:41 -0700

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -3,4 +3,19 @@
 int main(int argc, char *argv[])
 {
 	std::cout << "Hello world!" << std::endl;
+	std::string firstname, lastname;
+	std::int32_t age;
+	std::int32_t myage = 22;
+
+	std::cout << "What is your first name?";
+	std::cin >> firstname;
+	std::cout << "What is your last name?\n";
+	std::cin >> lastname;
+	std::cout << "How old are you?\n";
+	std::cin >> age;
+
+	std::cout << "Hello, " + firstname + " " + lastname + ". \n";
+	std::cout << "Your age is: " + age;
+	//int differance = myage - age;
+	//std::cout << "You are " + differance + " years older than me! \n";
 }
