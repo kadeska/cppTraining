d0914f0de0141398aa723ede612516a48ed577e1
kadeska <kadeska23@gmail.com>
2022-05-05 09:35:24 -0700

    Fixed combining Int and string together

--- src/main.cpp
+++ src/main.cpp
@@ -7,15 +7,15 @@ int main(int argc, char *argv[])
 	std::int32_t age;
 	std::int32_t myage = 22;
 
-	std::cout << "What is your first name?";
+	std::cout << "What is your first name?\n";
 	std::cin >> firstname;
 	std::cout << "What is your last name?\n";
 	std::cin >> lastname;
 	std::cout << "How old are you?\n";
 	std::cin >> age;
 
-	std::cout << "Hello, " + firstname + " " + lastname + ". \n";
-	std::cout << "Your age is: " + age;
-	//int differance = myage - age;
-	//std::cout << "You are " + differance + " years older than me! \n";
+	std::cout << "Hello, " << firstname << " " << lastname + ". \n";
+	std::cout << "Your age is: " << age;
+	int differance = myage - age;
+	std::cout << "You are " << differance << " years older than me! \n";
 }
