e4940a42e998303bb99d7799041a69a349ed4fd3
kadeska <kadeska23@gmail.com>
2022-05-06 20:04:27 -0700

    Implemented Vulken

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,35 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
 #include <iostream>
 
-int main(int argc, char *argv[])
-{
-	std::cout << "Hello world!" << std::endl;
-	std::string firstname, lastname;
-	std::int32_t age;
-	std::int32_t myage = 22;
+int main() {
+    glfwInit();
 
-	std::cout << "What is your first name?\n";
-	std::cin >> firstname;
-	std::cout << "What is your last name?\n";
-	std::cin >> lastname;
-	std::cout << "How old are you?\n";
-	std::cin >> age;
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
 
-	std::cout << "Hello, " << firstname << " " << lastname + ". \n";
-	std::cout << "Your age is: " << age;
-	int differance = myage - age;
-	std::cout << "You are " << differance << " years older than me! \n";
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    std::cout << extensionCount << " extensions supported\n";
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
+    return 0;
 }
