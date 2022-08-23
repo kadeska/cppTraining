128b9713824ee7e5e8046bdfb74357a7aa43a71d
kadeska <kadeska23@gmail.com>
2022-08-22 17:47:03 -0700

    Fixed typo in lve_window.hpp

--- src/lve_device.cpp
+++ src/lve_device.cpp
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8efbe9a586eeeb1e912301f66ba3ba64ddfcbf075c44c636a1ee8e02a77099a7
-size 18245
+oid sha256:ac1981092a2160eba103eded730d85e1e26a54109ac917100af423ea818be713
+size 19360
